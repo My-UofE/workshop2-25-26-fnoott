06c6f39b101d645eee24917fc1e87b6d29b7b58f
fnoott <fwn202@exeter.ac.uk>
2026-01-28 16:24:17 +0000

    HelloWorldApp

---
+++ HelloWorldApp.java
@@ -0,0 +1,11 @@
+// HelloWorld application
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.print("Hello World! ");
+        System.out.print("My name is " + args[0]);
+        System.out.print(". ");
+        System.out.println("I am " + args[1] + " years old.");
+    }
+}
+
+
